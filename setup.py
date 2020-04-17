6c6ca17c0398e5d35a86dc782366511516f0a1b0
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2020-04-17 12:55:36 +0200

    setup.py: exclude sentinel archive git repo, add version

--- setup.py
+++ setup.py
@@ -2,13 +2,21 @@ from pathlib import Path
 
 from setuptools import setup, find_namespace_packages
 
-requirements = Path("requirements.txt").read_text().strip().split("\n")
+requirements = [
+    p
+    for p in Path("requirements.txt").read_text().strip().split("\n")
+    if "github" not in p
+]
 
 # this is a namespace package for `docassemble`, see:
 # https://packaging.python.org/guides/packaging-namespace-packages/
 setup(
     name="docassemble-sarkui",
+    version="0.1.dev0",
+    description="A Q&A driven UI for SENTINEL archive",
     install_requires=requirements,
-    packages=find_namespace_packages(include=["docassemble.*"]),
+    packages=find_namespace_packages(
+        include=["docassemble.*"], exclude=["doc", "tests"],
+    ),
     package_data={"docassemble.sarkui": ["data/*"]},
 )
